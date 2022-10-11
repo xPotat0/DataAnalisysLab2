c93841f11188c9741ace07cb3278f2beed204de5
xPotat0 <106258306+xPotat0@users.noreply.github.com>
2022-10-11 16:59:21 +0500

    Add files via upload

---
+++ DataAnalysis.py
@@ -0,0 +1,19 @@
+import gspread
+import numpy as np
+gc = gspread.service_account(filename="unitydataanalisys-05a7991b08c6.json")
+sh = gc.open("UnitySheets")
+price = np.random.randint(2000, 10000, 11)
+mon = list(range(1,11))
+i = 0
+while i <= len(mon):
+    i += 1
+    if i == 0:
+        continue
+    else:
+        tempInf = ((price[i-1]-price[i-2])/price[i-2])*100
+        tempInf = str(tempInf)
+        tempInf = tempInf.replace('.', ',')
+        sh.sheet1.update(('A' + str(i)), str(i))
+        sh.sheet1.update(('B' + str(i)), str(price[i-1]))
+        sh.sheet1.update(('C' + str(i)), str(tempInf))
+        print(tempInf)
